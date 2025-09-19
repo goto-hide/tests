9ab6450716350b8b9a849004f7a26ff9779388d1
goto-hide <goto-hide@partner.kadokawa.jp>
2025-09-19 16:28:47 +0900

    init

---
+++ revertTest1/test.tf
@@ -0,0 +1,10 @@
+  records = []
+
+  aliases = [
+    {
+      name           = "test.ablog"
+      type           = "A"
+      target_name    = "XXXXX.cloudfront.net"
+      target_zone_id = "Z2FDTNDATAQYW2"
+    },
+  ]
